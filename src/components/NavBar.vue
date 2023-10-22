31ee19fea1d0beadf519198d01ba56ee05cbc06a
arkoa <arkoannuk+prog@gmail.com>
2023-10-22 13:10:31 +0300

    Feature: navbar to collapse on link clicks

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,15 @@
 <template>
   <nav class="navbar navbar-expand-md bg-body-secondary border-bottom">
     <div class="container-fluid">
-      <RouterLink class="navbar-brand" :to="{ name: 'Home' }">Playlists</RouterLink>
+      <RouterLink class="navbar-brand" :to="{ name: 'Home' }"
+        ><span
+          v-if="!isNavbarCollapsed"
+          data-bs-target="#navbar"
+          data-bs-toggle="collapse"
+          >Playlists</span
+        >
+        <span v-else>Playlists</span>
+      </RouterLink>
       <button
         class="navbar-toggler"
         type="button"
@@ -15,45 +23,52 @@
       </button>
       <div class="collapse navbar-collapse" id="navbar">
         <div class="navbar-nav ms-auto gap-0 gap-md-3">
-          <!-- <RouterLink class="nav-link" active-class="active" :to="{ name: 'Home' }"
-            >Home</RouterLink
-          > -->
           <RouterLink
-            class="nav-link"
+            class="nav-link py-0"
             active-class="active"
             :to="{ name: 'Create' }"
             v-if="user"
-            >Create Playlist</RouterLink
+            ><span data-bs-target="#navbar" data-bs-toggle="collapse" class="d-flex py-2"
+              >Create Playlist</span
+            ></RouterLink
           >
           <RouterLink
-            class="nav-link"
+            class="nav-link py-0"
             active-class="active"
             :to="{ name: 'UserPlaylists' }"
             v-if="user"
-            >My Playlists</RouterLink
+            ><span data-bs-target="#navbar" data-bs-toggle="collapse" class="d-flex py-2"
+              >My Playlists</span
+            ></RouterLink
           >
           <div
             type="button"
-            class="nav-link"
+            class="nav-link py-0"
             active-class="active"
             @click="handleClick"
             v-if="user"
           >
-            Logout
+            <span data-bs-target="#navbar" data-bs-toggle="collapse" class="d-flex py-2"
+              >Logout</span
+            >
           </div>
           <RouterLink
-            class="nav-link"
+            class="nav-link py-0"
             active-class="active"
             :to="{ name: 'Login' }"
             v-if="!user"
-            >Login</RouterLink
+            ><span data-bs-target="#navbar" data-bs-toggle="collapse" class="d-flex py-2"
+              >Login</span
+            ></RouterLink
           >
           <RouterLink
-            class="nav-link"
+            class="nav-link py-0"
             active-class="active"
             :to="{ name: 'Signup' }"
             v-if="!user"
-            >Signup</RouterLink
+            ><span data-bs-target="#navbar" data-bs-toggle="collapse" class="d-flex py-2"
+              >Signup</span
+            ></RouterLink
           >
         </div>
       </div>
@@ -73,10 +88,24 @@
 import useLogout from '@/composables/useLogout'
 import getUser from '@/composables/getUser'
 import { useRouter } from 'vue-router'
+import { onMounted, ref } from 'vue'
 
 const { user } = getUser()
 const router = useRouter()
 const { logout } = useLogout()
+const isNavbarCollapsed = ref(true)
+
+onMounted(() => {
+  const navbar = document.getElementById('navbar')
+  if (navbar) {
+    navbar.addEventListener('shown.bs.collapse', () => {
+      isNavbarCollapsed.value = false
+    })
+    navbar.addEventListener('hidden.bs.collapse', () => {
+      isNavbarCollapsed.value = true
+    })
+  }
+})
 
 async function handleClick() {
   await logout()
